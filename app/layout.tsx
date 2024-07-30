c6ac0e52115ec93e68b1fd22d49fc75634ecc529
Sohaib Furqan <sohaibfurqan@hotmail.com>
2024-07-30 16:13:48 +0100

    Adding Google analytics to nextjs app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
       <body className={inter.className}>{children}</body>
     </html>
   );
